c98163106d061917f4c1e56b5131984768d75255
Paul Stefko <ham2anv@gmail.com>
2022-12-28 09:48:32 -0500

    Added SQL script to create table.

---
+++ create_table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[MotionPicture] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (50)  NOT NULL,
+    [Description] NVARCHAR (500) NULL,
+    [Year]        INT            NOT NULL,
+    CONSTRAINT [PK_MotionPictures] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
